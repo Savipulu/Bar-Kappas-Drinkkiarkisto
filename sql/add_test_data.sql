cc4fc0a30921ee93e34998f366f2949179392dd8
Winter Jouni A A <jwinter@melkki.cs.helsinki.fi>
2015-11-09 00:09:28 +0200

    SQL-lauseiden muokkausta ja tyylien kokeilua

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -2,4 +2,5 @@
 INSERT INTO Drinker (name, password) VALUES ('Jouni', 'Jouni123'); -- Koska id-sarakkeen tietotyyppi on SERIAL, se asetetaan automaattisesti
 INSERT INTO Drinker (name, password) VALUES ('Vili', 'Vili123');
 INSERT INTO Drinker (name, password) VALUES ('Sasu', 'salasana1');
-INSERT INTO Drink (name, description, added) VALUES ('Rommikola', 'Rommia ja kolaa, duh!', NOW());
+INSERT INTO Drink (name, alcohol_content, volume, description) VALUES ('Rommikola', 8.5, 200, 'Rommi + kola + lime');
+INSERT INTO Ingredient (name, saldo, description) VALUES ('Rommi', 0.0, 'Suosittu alkoholijuoma');
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -25,11 +25,15 @@ description varchar(500)
 );
 
 CREATE TABLE Favourite (
+juoja integer NOT NULL,
+drinkki integer NOT NULL,
 FOREIGN KEY(juoja) REFERENCES Drinker(id),
 FOREIGN KEY(drinkki) REFERENCES Drink(id)
 );
 
 CREATE TABLE DrinkIngredient (
+drinkki integer NOT NULL,
+ainesosa integer NOT NULL,
 FOREIGN KEY(drinkki) REFERENCES Drink(id),
 FOREIGN KEY(ainesosa) REFERENCES Ingredient(id)
 );
