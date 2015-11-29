d5b5ebd3856fd7c03d4e7e99317278c1f698c976
Syaniidikauppias <jouni.winter@gmail.com>
2015-11-30 01:35:40 +0200

    dokumentaation päivitys komponenttikaaviolla

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -4,7 +4,7 @@
 
     public static function index(){
       // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  View::make('frontpage.html');
+   	  View::make('index.html');
     }
 
     public static function sandbox(){
